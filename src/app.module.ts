1f053dd241bbe3d239cbfca2d2e350170634783f
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-01-07 00:55:53 +0200

    main modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common";
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-    imports: [],
+    imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
     controllers: [],
     providers: [],
 })
