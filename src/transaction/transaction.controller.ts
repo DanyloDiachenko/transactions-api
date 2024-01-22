3455cb4dd05025ee9156d75d1eade163c135eeeb
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-01-22 22:47:05 +0200

    transactions full end

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,34 +1,71 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TransactionService } from './transaction.service';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	UsePipes,
+	ValidationPipe,
+	UseGuards,
+	Req,
+	Query,
+} from "@nestjs/common";
+import { TransactionService } from "./transaction.service";
+import { CreateTransactionDto } from "./dto/create-transaction.dto";
+import { UpdateTransactionDto } from "./dto/update-transaction.dto";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 
-@Controller('transaction')
+@Controller("transaction")
 export class TransactionController {
-  constructor(private readonly transactionService: TransactionService) {}
+	constructor(private readonly transactionService: TransactionService) {}
 
-  @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionService.create(createTransactionDto);
-  }
+	@Post()
+	@UsePipes(new ValidationPipe())
+	@UseGuards(JwtAuthGuard)
+	create(@Body() createTransactionDto: CreateTransactionDto, @Req() req) {
+		return this.transactionService.create(
+			createTransactionDto,
+			req.user.id,
+		);
+	}
 
-  @Get()
-  findAll() {
-    return this.transactionService.findAll();
-  }
+	@Get("pagination")
+	@UseGuards(JwtAuthGuard)
+	findAllWithPagination(
+		@Req() req,
+		@Query("page") page: number,
+		@Query("limit") limit: number,
+	) {
+		return this.transactionService.findAllWithPagination(
+			+req.user.id,
+			+page,
+			+limit,
+		);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
-  }
+	@Get()
+	@UseGuards(JwtAuthGuard)
+	findAll(@Req() req) {
+		return this.transactionService.findAll(+req.user.id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionService.update(+id, updateTransactionDto);
-  }
+	@Get(":id")
+	findOne(@Param("id") id: string) {
+		return this.transactionService.findOne(+id);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
-  }
+	@Patch(":id")
+	update(
+		@Param("id") id: string,
+		@Body() updateTransactionDto: UpdateTransactionDto,
+	) {
+		return this.transactionService.update(+id, updateTransactionDto);
+	}
+
+	@Delete(":id")
+	remove(@Param("id") id: string) {
+		return this.transactionService.remove(+id);
+	}
 }
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,26 +1,113 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+} from "@nestjs/common";
+import { CreateTransactionDto } from "./dto/create-transaction.dto";
+import { UpdateTransactionDto } from "./dto/update-transaction.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Transaction } from "./entities/transaction.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class TransactionService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
-  }
+	constructor(
+		@InjectRepository(Transaction)
+		private readonly transactionRepository: Repository<Transaction>,
+	) {}
 
-  findAll() {
-    return `This action returns all transaction`;
-  }
+	async create(createTransactionDto: CreateTransactionDto, id: number) {
+		const newTransaction = {
+			title: createTransactionDto.title,
+			amount: createTransactionDto.amount,
+			type: createTransactionDto.type,
+			category: { id: +createTransactionDto.category },
+			user: { id },
+		};
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
-  }
+		if (!newTransaction) {
+			throw new BadRequestException("Something went wrong");
+		}
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
+		return await this.transactionRepository.save(newTransaction);
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
-  }
+	async findAll(id: number) {
+		const transactions = await this.transactionRepository.find({
+			where: {
+				user: {
+					id,
+				},
+			},
+			order: {
+				createdAt: "DESC",
+			},
+		});
+
+		return transactions;
+	}
+
+	async findOne(id: number) {
+		const transaction = await this.transactionRepository.findOne({
+			where: {
+				id,
+			},
+			relations: {
+				user: true,
+				category: true,
+			},
+		});
+
+		if (!transaction) {
+			throw new NotFoundException("Transaction not found");
+		}
+
+		return transaction;
+	}
+
+	async update(id: number, updateTransactionDto: UpdateTransactionDto) {
+		const transaction = await this.transactionRepository.findOne({
+			where: { id },
+		});
+
+		if (!transaction) {
+			throw new NotFoundException("Transaction not found");
+		}
+
+		return await this.transactionRepository.update(
+			id,
+			updateTransactionDto,
+		);
+	}
+
+	async remove(id: number) {
+		const transaction = await this.transactionRepository.findOne({
+			where: { id },
+		});
+
+		if (!transaction) {
+			throw new NotFoundException("Transaction not found");
+		}
+
+		return await this.transactionRepository.delete(id);
+	}
+
+	async findAllWithPagination(id: number, page: number, limit: number) {
+		const transactions = await this.transactionRepository.find({
+			where: {
+				user: { id },
+			},
+			relations: {
+				category: true,
+				user: true,
+			},
+			order: {
+				createdAt: "DESC",
+			},
+			take: limit,
+			skip: (page - 1) * limit,
+		});
+
+		return transactions;
+	}
 }
