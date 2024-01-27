a030c747da2abfe08c4b772ff6d359ff7f6515c4
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-01-27 14:08:12 +0200

    onDelete relation fix

--- src/transaction/entities/transaction.entity.ts
+++ src/transaction/entities/transaction.entity.ts
@@ -1,40 +1,42 @@
 import { Category } from "src/category/entities/category.entity";
 import { User } from "src/user/entities/user.entity";
 import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
+	Column,
+	CreateDateColumn,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
 } from "typeorm";
 
 @Entity()
 export class Transaction {
-    @PrimaryGeneratedColumn({ name: "transaction_id" })
-    id: number;
+	@PrimaryGeneratedColumn({ name: "transaction_id" })
+	id: number;
 
-    @Column()
-    title: string;
+	@Column()
+	title: string;
 
-    @Column()
-    amount: number;
+	@Column()
+	amount: number;
 
-    @Column({ nullable: true })
-    type: string;
+	@Column({ nullable: true })
+	type: string;
 
-    @ManyToOne(() => User, (user) => user.transactions)
-    @JoinColumn({ name: "user_id" })
-    user: User;
+	@ManyToOne(() => User, (user) => user.transactions)
+	@JoinColumn({ name: "user_id" })
+	user: User;
 
-    @ManyToOne(() => Category, (category) => category.transactions)
-    @JoinColumn({ name: "category_id" })
-    category: Category;
+	@ManyToOne(() => Category, (category) => category.transactions, {
+		onDelete: "SET NULL",
+	})
+	@JoinColumn({ name: "category_id" })
+	category: Category;
 
-    @CreateDateColumn()
-    createdAt: Date;
+	@CreateDateColumn()
+	createdAt: Date;
 
-    @UpdateDateColumn()
-    updateAt: Date;
+	@UpdateDateColumn()
+	updateAt: Date;
 }
