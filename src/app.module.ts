5a3f7c58a3df8a3710e6d8974c400829543f6040
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-07-30 17:51:03 +0300

    _

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,30 +7,30 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-        }),
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: (configService: ConfigService) => ({
-                type: "postgres",
-                host: configService.get("DB_HOST"),
-                port: configService.get<number>("DB_PORT"),
-                username: configService.get("DB_USERNAME"),
-                password: configService.get("DB_PASSWORD"),
-                database: configService.get("DB_NAME"),
-                synchronize: true,
-                autoLoadEntities: true,
-            }),
-            inject: [ConfigService],
-        }),
-        UserModule,
-        CategoryModule,
-        AuthModule,
-        TransactionModule,
-    ],
-    controllers: [],
-    providers: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				type: "postgres",
+				host: configService.get("DB_HOST"),
+				port: configService.get<number>("DB_PORT"),
+				username: configService.get("DB_USERNAME"),
+				password: configService.get("DB_PASSWORD"),
+				database: configService.get("DB_NAME"),
+				synchronize: true,
+				autoLoadEntities: true,
+			}),
+			inject: [ConfigService],
+		}),
+		UserModule,
+		CategoryModule,
+		AuthModule,
+		TransactionModule,
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
